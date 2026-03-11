82974438afabc354626b4cea4feb4e4b957d2aab
Genny4Real1 <and.russo.0511@gmail.com>
2026-03-11 16:05:32 +0100

    New: scripts and helicopter input actions

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,17 +1,58 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
 
 
 public class PlayerControls : MonoBehaviour
 {
+    public GameObject mainRotor;
+    public GameObject secondaryRotor;
+    
+    // Engine Control Vars
+    public float maxEngineSpeed = 7000.0f;
+    private float currentEngineSpeed = 0.0f;
+    public bool engineActive = true;
+    // Main Rotor Vars
+    public bool mainRotorActive = true;
+    public float maxMainRotorTorque = 10000.0f;
+    private float currentMainRotorTorque = 0.0f;
+    // Secondary Rotor Vars
+    public bool secondaryRotorActive = true;
+    public float maxSecondaryRotorTorque = 10000.0f;
+    private float currentSecondaryRotorTorque = 0.0f;
+    // Velocity Vars
+    public float maxMainRotorVelocity = 10000.0f;
+    public float maxSecondaryRotorVelocity = 10000.0f;
+    // Control Modifiers
+    public float forwardRotorTorqueModifier = 1.0f;
+    public float sidewayRotorTorqueModifier = 1.0f;
+    
+    private PlayerInput playerInput;
+    private InputAction throttleAction;
+    private InputAction cyclicAction;
+    private InputAction collectiveAction;
+    private InputAction pedalAction;
+    
+    private Rigidbody rb;
+    private AudioSource audioSource;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    // FixedUpdate is used to calculate things that cannot be dependent on FPS 
+    private void FixedUpdate()
+    {
     }
 }
