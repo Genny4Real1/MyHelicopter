838c00bef0c20802ab07e03a8523a84219f5bfac
Genny4Real1 <and.russo.0511@gmail.com>
2026-03-19 18:00:00 +0100

    New 3/19 #02 END OF LESSON
    - Secondary Rotor rotation
    
    - Secondary Rotor Torque logic added
    
    - Cyclic Torque logic added
    
    - General fixed to code readability and optimization

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -16,23 +16,26 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] GameObject secondaryRotor;
     
     [Header("Engine Control Vars")]
-    public float maxEngineSpeed = 7000.0f;
+    // public float maxEngineSpeed = 2800.0f; // RPM
     public bool engineActive = false;
     [SerializeField] [Range(0f, 1f)] float currentEngineSpeed = 0f;
     [Header("Main Rotor Vars")]
     public bool mainRotorActive = true;
     public float maxMainRotorTorque = 2000.0f;
+    public float maxCyclicForce = 1200f;
     [SerializeField] float currentMainRotorTorque;
+    [SerializeField] Vector2 currentCyclicForce;
     [Header("Secondary Rotor Vars")]
     public bool secondaryRotorActive = true;
     public float maxSecondaryRotorTorque = 400.0f;
-    [SerializeField] [Range(0f, 1f)] float currentSecondaryRotorTorque = 0.0f;
+    [SerializeField] float currentSecondaryRotorTorque = 0.0f;
     [Header("Velocity Vars")]
-    public float maxMainRotorVelocity = 10000.0f;
-    public float maxSecondaryRotorVelocity = 10000.0f;
+    public float maxMainRotorVelocity = 10000.0f; // RPM
+    public float maxSecondaryRotorVelocity = 10000.0f; // RPM
     [Header("Control Modifiers")]
-    public float forwardRotorTorqueModifier = 1.0f;
-    public float sidewayRotorTorqueModifier = 1.0f;
+    [SerializeField] float forwardRotorTorqueModifier = 1.0f;
+    [SerializeField] float sidewayRotorTorqueModifier = 0.25f;
+    [SerializeField] float cyclicInputModifier = 1.0f;
     
     // private PlayerInput playerInput;
     private Helicopter_Actions helicopterActions;
@@ -89,12 +92,17 @@ public class PlayerControls : MonoBehaviour
         if (engineAction != null && engineAction.WasPressedThisFrame())
         {
             engineActive = !engineActive;
-            Debug.Log($"Engine active = {engineActive}");
         }
 
         if (mainRotorActive)
         {
-            mainRotor.transform.rotation *= Quaternion.Euler(0, maxMainRotorVelocity * currentEngineSpeed * Time.deltaTime, 0);
+            mainRotor.transform.rotation *= Quaternion.Euler(0, maxMainRotorVelocity * currentEngineSpeed * Time.deltaTime * 6f, 0);
+        }
+
+        if (secondaryRotorActive)
+        {
+            secondaryRotor.transform.rotation *= Quaternion.Euler(0, (maxSecondaryRotorVelocity * currentEngineSpeed * 6f) * Time.deltaTime, 0);
+
         }
     }
 
@@ -102,14 +110,13 @@ public class PlayerControls : MonoBehaviour
     private void FixedUpdate()
     {
         var throttle = throttleAction.ReadValue<float>();
-        var cyclic = cyclicAction.ReadValue<float>();
+        var cyclic = cyclicAction.ReadValue<Vector2>();
         var collective = collectiveAction.ReadValue<float>();
         var pedal = pedalAction.ReadValue<float>();
         
         if (engineActive)
         {
             currentEngineSpeed = Mathf.Clamp(currentEngineSpeed + throttle * Time.fixedDeltaTime, 0.0f, 1.2f);
-            Debug.Log("Engine Speed = " + currentEngineSpeed);
         }
         else
         {
@@ -118,9 +125,18 @@ public class PlayerControls : MonoBehaviour
 
         if (mainRotorActive)
         {
+            
             currentMainRotorTorque = collective * maxMainRotorTorque * currentEngineSpeed * forwardRotorTorqueModifier;
-            Debug.Log("Main Rotor Torque = " + currentMainRotorTorque); 
             rb.AddRelativeForce(Vector3.up * currentMainRotorTorque);
+
+            currentCyclicForce = cyclic * (maxCyclicForce * currentEngineSpeed * cyclicInputModifier);
+            rb.AddRelativeTorque(new Vector3(-currentCyclicForce.y, 0, currentCyclicForce.x));
+        }
+        
+        if (secondaryRotorActive)
+        {
+            currentSecondaryRotorTorque = pedal * maxSecondaryRotorTorque * currentEngineSpeed * sidewayRotorTorqueModifier;
+            rb.AddRelativeTorque(Vector3.up * currentSecondaryRotorTorque);
         }
     }
 }
